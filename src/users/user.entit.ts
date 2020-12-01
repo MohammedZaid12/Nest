610eb24d0c57d591e124ebead300b3dfa0de24cd
MohammedZaid12 <zaid1805a@aptechgdn.net>
2020-11-30 22:41:48 -0800

    auth module

--- src/users/user.entit.ts
+++ src/users/user.entit.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import * as bcrypt from 'bcryptjs'
 
-@Entity()
+@Entity('user')
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
@@ -8,5 +9,23 @@ export class User {
     @Column({ length: 25 })
     fullName:string;
 
+    @Column({ type:'text' , unique:true })
+    email:string;
+    
+    @Column()
+    password:string;
+    
+   @CreateDateColumn()
+   createdDate: Date;
+
+   @BeforeInsert()
+    async hashPassword(){
+       this.password = await bcrypt.hash(this.password , 10);
+   }
+
+   toResponseObject(){
+    const {id,fullName,createdDate,email} = this;
+    return {id,fullName,createdDate,email};
+   }
    
 }
